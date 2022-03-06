71fae96075fa2aa61b13dd048d6aebd4f6c5d807
hszzz <hszzz.me@gmail.com>
2022-03-06 15:33:15 +0800

    feat: socket wrap

---
+++ socket_wrap.cc
@@ -0,0 +1,28 @@
+#include "socket_wrap.h"
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+namespace sockets {
+
+int socket_or_die() {
+  int sockfd = ::socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    ::exit(-1);
+  }
+  return sockfd;
+}
+
+void bind_or_die(int sockfd, const sockaddr* addr) {
+  int ret = ::bind(sockfd, addr, static_cast<socklen_t>(sizeof(sockaddr_in)));
+  if (ret < 0) ::exit(-1);
+}
+
+void listen_or_die(int sockfd) {
+  int ret = ::listen(sockfd, SOMAXCONN);
+  if (ret < 0) ::exit(-1);
+}
+
+} // namespace sockets
+
---
+++ socket_wrap.h
@@ -0,0 +1,15 @@
+#ifndef SOCKET_WRAP_H
+#define SOCKET_WRAP_H
+
+#include <arpa/inet.h>
+
+namespace sockets {
+ 
+int socket_or_die();
+void bind_or_die(int sockfd, const sockaddr* addr);
+void listen_or_die(int sockfd);
+
+} // namespace sockets
+
+#endif// SOCKET_WRAP_H
+

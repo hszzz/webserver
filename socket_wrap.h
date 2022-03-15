8a2a3ab946fc1bdd38d9ccf22c95ecbc759f23d4
hszzz <hszzz.me@gmail.com>
2022-03-15 22:24:01 +0800

    style:

--- socket_wrap.h
+++ socket_wrap.h
@@ -4,13 +4,12 @@
 #include <arpa/inet.h>
 
 namespace sockets {
- 
+
 int socket_or_die();
 void bind_or_die(int sockfd, const sockaddr* addr);
 void listen_or_die(int sockfd);
 int accept(int sockfd, sockaddr_in* addr);
 
-} // namespace sockets
+}  // namespace sockets
 
-#endif// SOCKET_WRAP_H
-
+#endif  // SOCKET_WRAP_H

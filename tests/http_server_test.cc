df68f73aa2f2c89feb6f21c998502f0a9cfc3eb9
hszzz <hszzz.me@gmail.com>
2022-03-25 22:41:02 +0800

    test: test http server

--- tests/http_server_test.cc
+++ tests/http_server_test.cc
@@ -1,9 +1,17 @@
 #include "../http_server.h"
 
+void HttpCallBack(http::HttpRequest* req, http::HttpResponse* res) {
+  if (req->GetMethod() == http::HttpMethod::kGet) {
+    res->SetStatus(http::HttpStatus::kOK);
+    res->SetMessage("200 OK");
+    res->SetBody("<h1>hello world</h1>");
+  }
+}
+
 int main() {
   http::HttpServer server;
   server.SetPort(8888);
+  server.SetHttpCallBack(HttpCallBack);
   server.start();
   return 0;
 }
-

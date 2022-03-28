a0636d229df079194496807c0663b5b2034fb885
hszzz <hszzz.me@gmail.com>
2022-03-28 20:45:25 +0800

    tests: add test

--- tests/http_server_test.cc
+++ tests/http_server_test.cc
@@ -1,10 +1,18 @@
 #include "../http_server.h"
 
 void HttpCallBack(http::HttpRequest* req, http::HttpResponse* res) {
-  if (req->GetMethod() == http::HttpMethod::kGet) {
+  if (req->GetPath() == "/") {
     res->SetStatus(http::HttpStatus::kOK);
-    res->SetMessage("200 OK");
+    res->SetMessage("OK");
     res->SetBody("<h1>hello world</h1>");
+  } else if (req->GetPath() == "/ping") {
+    res->SetStatus(http::HttpStatus::kOK);
+    res->SetMessage("OK");
+    res->SetBody("pong");
+  } else if (req->GetPath() == "/index.html") {
+    res->SetStatus(http::HttpStatus::kOK);
+    res->SetMessage("OK");
+    res->SetBodyFromFile("/home/hszzz/webserver/docs/index.html");
   }
 }
 

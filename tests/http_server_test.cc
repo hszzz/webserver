7639073d55fee0e4ce8470c2c4f6d196dcfed2b3
hszzz <hszzz.me@gmail.com>
2022-04-06 20:40:24 +0800

    test: http server

--- tests/http_server_test.cc
+++ tests/http_server_test.cc
@@ -1,18 +1,28 @@
 #include "../http_server.h"
 
 void HttpCallBack(http::HttpRequest* req, http::HttpResponse* res) {
-  if (req->GetPath() == "/") {
-    res->SetStatus(http::HttpStatus::kOK);
-    res->SetMessage("OK");
-    res->SetBody("<h1>hello world</h1>");
-  } else if (req->GetPath() == "/ping") {
-    res->SetStatus(http::HttpStatus::kOK);
-    res->SetMessage("OK");
-    res->SetBody("pong");
-  } else if (req->GetPath() == "/index.html") {
-    res->SetStatus(http::HttpStatus::kOK);
-    res->SetMessage("OK");
-    res->SetBodyFromFile("/home/hszzz/webserver/docs/index.html");
+  if (req->GetMethod() == http::HttpMethod::kGet) {
+    if (req->GetPath() == "/") {
+      res->SetStatus(http::HttpStatus::kOK);
+      res->SetMessage("OK");
+      res->SetBody("<h1>hello world</h1>");
+    } else if (req->GetPath() == "/ping") {
+      res->SetStatus(http::HttpStatus::kOK);
+      res->SetMessage("OK");
+      res->SetBody("pong");
+    } else if (req->GetPath() == "/index.html") {
+      res->SetStatus(http::HttpStatus::kOK);
+      res->SetMessage("OK");
+      res->SetBodyFromFile("/home/hszzz/webserver/docs/index.html");
+    } else {
+      res->SetStatus(http::HttpStatus::kNotFound);
+      res->SetMessage("Page Not Found");
+      res->SetBody("Page Not Found!!!");
+    }
+  } else if (req->GetMethod() == http::HttpMethod::kPost) {
+  } else {
+    res->SetStatus(http::HttpStatus::kBadRequest);
+    res->SetMessage("bad request");
   }
 }
 
